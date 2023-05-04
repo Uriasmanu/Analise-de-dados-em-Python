2a04c24cdab8a56aa50183be0a94d2cb088a027f
Manoela Urias <maanoelaurias@gmail.com~>
2023-05-04 10:36:48 -0300

    atividades

---
+++ exemplo2.py
@@ -0,0 +1,3 @@
+nome = input("Digite um seu nome: ")
+
+print(f"Olá {nome}, bem vindo a disciplina de programação. Parabéns pelo seu primeiro hello world")
---
+++ operacao.py
@@ -0,0 +1,24 @@
+operacao_1 = 2 + 3 * 5
+
+operacao_2 = (2 + 3) * 5
+
+operacao_3 = 4 / 2 ** 2
+
+operacao_4 = 13 % 3 + 4
+
+quociente = 6 / 2
+parteInteira_quociente = 6 // 2
+abs(6)
+
+print(f"Resultado em operacao_1 = {operacao_1}")
+print(f"Resultado em operacao_2 = {operacao_2}")
+print(f"Resultado em operacao_3 = {operacao_3}")
+print(f"Resultado em operacao_4 = {operacao_4}")
+print(f"Resultado em quociente = {quociente}")
+print(f"Resultado em Parte inteira de quociente = {parteInteira_quociente}")
+print(f"Resultado em absoluto = {abs(6)}")
+
+
+
+
+
